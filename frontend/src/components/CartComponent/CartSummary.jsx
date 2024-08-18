a24037cf10f2ab46f22d82b19c9cd9e988452aca
wicaksonoleksono <wcksnlxn@gmail.com>
2024-08-18 12:31:22 +0700

    fixing: layout context

--- frontend/src/components/CartComponent/CartSummary.jsx
+++ frontend/src/components/CartComponent/CartSummary.jsx
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useState } from "react";
 
-const CartSummary = ({ total }) => {
-  const formattedTotal = total.toLocaleString('id-ID') + ',00';
+const CartSummary = ({ total, onApplyPromotion }) => {
+  const [voucherCode, setVoucherCode] = useState("");
+
+  const handleApply = () => {
+    if (voucherCode) {
+      onApplyPromotion(voucherCode);
+    } else {
+      alert("Please enter a voucher code.");
+    }
+  };
+
+  const formattedTotal = total.toLocaleString("id-ID") + ",00";
 
   return (
     <div className="p-4 bg-white text-black rounded shadow">
       <h3 className="font-semibold text-lg mb-4">Ringkasan Belanja</h3>
-      <p className="font-semibold text-gray-700 mb-2">Total: Rp{formattedTotal}</p>
-      <button className="w-full py-2 bg-green-500  rounded">Beli</button>
+      <p className="font-semibold text-gray-700 mb-2">
+        Total: Rp{formattedTotal}
+      </p>
+      <input
+        type="text"
+        value={voucherCode}
+        onChange={(e) => setVoucherCode(e.target.value)}
+        placeholder="Voucher Code"
+        className="w-full p-2 mb-2 border rounded"
+      />
+      <button
+        onClick={handleApply}
+        className="w-full py-2 bg-blue-500 rounded mb-2"
+      >
+        Apply Promotion
+      </button>
+      <button className="w-full py-2 bg-green-500 rounded">Beli</button>
     </div>
   );
 };
--- frontend/src/pages/Profile/index.jsx
+++ frontend/src/pages/Profile/index.jsx
@@ -1,13 +1,12 @@
-import React, { useState } from 'react';
-import Layout from '@/components/LayoutComponent/Layout';
-import UserProfile from '@/components/UserComponent/UserProfile';
+import React, { useState } from "react";
+import Layout from "@/components/LayoutComponent/Layout";
+import UserProfile from "@/components/UserComponent/UserProfile";
 
 const Index = () => {
-
   return (
-    <Layout className="login-home bg-white">
+    <div className="login-home bg-white">
       <UserProfile />
-    </Layout>
+    </div>
   );
 };
 
