3693edd34e44224051ebc352432174f4dc588642
davidfrdian <dferdian11@gmail.com>
2024-08-23 17:14:05 +0700

    delete manage order

--- frontend/src/components/SellerLayoutComponent/Dashboard.jsx
+++ frontend/src/components/SellerLayoutComponent/Dashboard.jsx
@@ -8,22 +8,7 @@ const Dashboard = () => {
   return (
     <div className="max-w-6xl mx-auto p-6 bg-white shadow-md rounded-md">
       <h2 className="text-2xl font-bold mb-6">Seller Dashboard</h2>
-      <div className="flex mb-6">
-        <button
-          className={`mr-4 p-2 ${activeTab === 'products' ? 'bg-indigo-600 text-white' : 'bg-gray-200'}`}
-          onClick={() => setActiveTab('products')}
-        >
-          Create Products
-        </button>
-        <button
-          className={`mr-4 p-2 ${activeTab === 'orders' ? 'bg-indigo-600 text-white' : 'bg-gray-200'}`}
-          onClick={() => setActiveTab('orders')}
-        >
-          Manage Orders
-        </button>
-      </div>
       {activeTab === 'products' && <ManageProduct />}
-      {activeTab === 'orders' && <ManageOrders />}
     </div>
   );
 };
