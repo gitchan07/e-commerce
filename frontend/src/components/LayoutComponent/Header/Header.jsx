4e4c44e604c972201c879b5c18afd7f05f01c191
gitchan07 <gitasandria00@gmail.com>
2024-08-23 14:10:49 +0700

    revisi seller dashboard

--- frontend/src/components/LayoutComponent/Header/Header.jsx
+++ frontend/src/components/LayoutComponent/Header/Header.jsx
@@ -57,7 +57,7 @@ const Header = () => {
           {isSeller ? (
             <>
               <Link
-                href="/Seller"
+                href="/products"
                 className="px-4 py-2 bg-gradient-to-r from-sky-600 to-sky-400 text-white rounded-xl hover:bg-gradient-to-r hover:from-sky-700 hover:to-sky-500 shadow-lg transition-transform transform hover:scale-105"
               >
                 Seller Dashboard
